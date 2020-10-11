9077ddbfd217a502e5b50f24a401e26b8d02e170
MAbdurahman <mdbdrrhm@gmail.com>
2020-10-11 05:42:19 -0500

    Add jquery, bootstrap, and modify index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
