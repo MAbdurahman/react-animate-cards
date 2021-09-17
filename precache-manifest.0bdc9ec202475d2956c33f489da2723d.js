65a59cacd0c4892480beb4abfec9978e53bee16f
MAbdurahman <mdbdrrhm@gmail.com>
2021-09-17 04:23:49 -0500

    Updates

---
+++ precache-manifest.0bdc9ec202475d2956c33f489da2723d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fb929442612a5076ac740ccbaa9e67e",
+    "url": "/react-animate-cards/index.html"
+  },
+  {
+    "revision": "a9e323baa827cc63fe5c",
+    "url": "/react-animate-cards/static/css/2.8e1873ca.chunk.css"
+  },
+  {
+    "revision": "62e95d038f862f4fa5cb",
+    "url": "/react-animate-cards/static/css/main.ce71c629.chunk.css"
+  },
+  {
+    "revision": "a9e323baa827cc63fe5c",
+    "url": "/react-animate-cards/static/js/2.2481f5ca.chunk.js"
+  },
+  {
+    "revision": "4287661eb5fd89dd167e6f815d78395c",
+    "url": "/react-animate-cards/static/js/2.2481f5ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62e95d038f862f4fa5cb",
+    "url": "/react-animate-cards/static/js/main.1ff78c3b.chunk.js"
+  },
+  {
+    "revision": "f2a359edca3eb3f5d2a5",
+    "url": "/react-animate-cards/static/js/runtime-main.7f7652a7.js"
+  }
+]);
